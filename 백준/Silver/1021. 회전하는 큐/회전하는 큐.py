dacf58282ace6b017c6ff1e711388b2cdccef11e
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-05-03 23:39:23 +0900

    [Silver III] Title: 회전하는 큐, Time: 76 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+N, M = map(int, input().split())
+deq = deque([i for i in range(1, N+1)])
+idx = list(map(int, input().split()))
+
+cnt = 0
+for i in idx:
+    while True:
+        if deq[0] == i:
+            deq.popleft()
+            break
+
+        else:
+            if deq.index(i) <= len(deq)//2:
+                deq.rotate(-1)
+                cnt += 1
+            else:
+                deq.rotate(1)
+                cnt += 1
+
+print(cnt)
