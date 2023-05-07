4daba55c6c0dae7938b2710f83bc4628a571ca87
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-05-07 22:20:35 +0900

    [Gold V] Title: AC, Time: 276 ms, Memory: 154168 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,34 @@
+import sys
+# sys.stdin = open('input/5430_input.txt')
+input = sys.stdin.readline
+from collections import deque
+
+T = int(input())
+for _ in range(T):
+    order = input().rstrip()
+    N = int(input())
+    deq = deque(input().rstrip()[1:-1].split(','))
+
+    if N == 0:
+        deq.clear()
+
+    flag = 1
+    cnt = 0
+    for i in range(len(order)):
+        if order[i] == 'R':
+            cnt += 1
+        else:
+            if len(deq) and cnt % 2 == 0:
+                deq.popleft()
+            elif len(deq) and cnt % 2:
+                deq.pop()
+            else:
+                flag = 0
+
+    if cnt % 2:
+        deq.reverse()
+
+    if flag:
+        print('['+','.join(deq)+']')
+    else:
+        print('error')
