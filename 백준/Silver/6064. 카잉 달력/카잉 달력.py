746526087e223cf34d61d05b6e77221a67ac8009
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-05-16 22:44:56 +0900

    [Silver I] Title: 카잉 달력, Time: 1448 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,12 @@
+def solve(M, N, x, y):
+    while x <= M * N:
+        if (x - y) % N == 0:
+            return x
+        x += M
+
+    return -1
+
+T = int(input())
+for _ in range(T):
+    M, N, x, y = map(int, input().split())
+    print(solve(M, N, x, y))
