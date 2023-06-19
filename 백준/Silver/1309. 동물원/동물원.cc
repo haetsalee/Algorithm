facdc195e54a3bd511ceccfde10e793e9ee8b962
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-06-20 03:09:26 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    
+    int dp[100001];
+    dp[0] = 1;
+    dp[1] = 3;
+    
+    for(int i=2; i<=N; i++)
+        dp[i]=(dp[i-1]*2 + dp[i-2]) % 9901;
+        
+    cout << dp[N];
+}
