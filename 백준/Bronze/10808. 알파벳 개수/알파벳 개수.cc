337976fc57a61036719f809c939543fa4222c738
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-07-03 02:38:03 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	
+	int arr[26] = {0, };
+	
+	for (int i = 0; i < str.size(); i++) {
+		arr[str[i] - 97] += 1;
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << arr[i]<<" ";
+	}
+}
