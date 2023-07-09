14db31942c345ae93504f2f2b59d371acb3ddd58
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-07-10 02:08:25 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -7,10 +7,8 @@ int main()
     int N;
     cin >> N;
     
-    for (int i=0; i<N; i++)
-    {
-        for (int j=N; j>i; j--)
-        {
+    for (int i=0; i<N; i++) {
+        for (int j=N; j>i; j--) {
             cout << "*";
         }
         cout << "\n";
