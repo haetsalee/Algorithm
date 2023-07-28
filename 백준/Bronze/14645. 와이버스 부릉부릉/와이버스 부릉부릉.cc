3f035734578aa4cb9018a908b0364969d4fe9567
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-07-29 01:15:23 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, K, A, B;
+    cin >> N >> K;
+    for (int i=0; i<N; i++) {
+        cin >> A >> B;
+    }
+    
+    cout << "비와이";
+}
