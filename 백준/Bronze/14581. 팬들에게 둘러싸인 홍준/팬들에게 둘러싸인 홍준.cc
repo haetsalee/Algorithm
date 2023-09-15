477febe44766a737fe08d41cc34e88c251014271
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-09-15 22:29:45 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	string name;
+	cin >> name;
+
+	char arr[6] = ":fan:";
+
+	for (int i=0; i<3; i++) {
+		for (int j=0; j<3; j++) {
+			if (i == 1 && j == 1) 
+				cout << ":" << name << ":";
+
+			else
+				cout << arr;
+		}
+
+		cout << "\n";
+	}
+}
