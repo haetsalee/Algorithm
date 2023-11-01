f5a03ebd27c7c9bab0a064babf61b9664476fe21
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-11-01 21:49:45 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    while (N--) {
+        long long A, B;
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+}
