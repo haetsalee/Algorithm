f6cf45fd10b62ac838f1bd25d78da9b8d3511c47
leehaetsal <109335423+haetsalee@users.noreply.github.com>
2023-12-15 12:10:08 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int num;
+        cin >> num;
+        
+        for (int i=0; i<num; i++) {
+            cout << "=";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
